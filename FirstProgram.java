10e7ba4f87fe869411e132fa3a59958dff16d9d3
Reshmitha29 <29reshmithavaishnaviv@gmail.com>
2024-12-14 16:59:13 +0530

    Update and rename Test to FirstProgram.java
    This is my first program .

---
+++ FirstProgram.java
@@ -0,0 +1,8 @@
+class FirstProgram{
+void printing(){
+        System.out.println("My First Java Program.");
+    }
+  public static void main(String []args){
+FirstProgram pg1 = new FirstProgram();
+pg1.printing();
+};
